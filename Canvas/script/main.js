35c06bd95139f0e028bb9985bb4595074568f50d
girishvr <girish.vin.r@gmail.com>
2016-04-01 10:40:33 +0530

    Added basic files

---
+++ Canvas/script/main.js
@@ -0,0 +1,39 @@
+var app = playground({
+
+  create: function() {
+  
+  },
+
+  ready: function() {
+
+  },
+
+  step: function(delta) {
+
+  },
+
+  render: function(delta) {
+      
+  },
+
+  mousedown: function(event) {
+
+  },
+
+  mouseup: function(event) {
+
+  },
+
+  mousemove: function(event) {
+
+  },
+
+  keydown: function(event) {
+
+  },
+
+  keyup: function(event) {
+
+  }
+
+});
